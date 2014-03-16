2bb4a95e633a48315b362ca6e05fd6ac55d4eb65
Mahendra Dash <mahendra_k_dash@yahoo.com>
2014-03-16 20:18:01 +0530

    Tests put in proper package. Included TestNG library.

---
+++ src/test/java/com/learn/selenium/gmail/inbox/ReadMailTest.java
@@ -0,0 +1,34 @@
+package com.learn.selenium.gmail.inbox;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ReadMailTest {
+	WebDriver driver;
+	@Test
+	public void readMailTest() {
+		driver = new FirefoxDriver();
+		driver.navigate().to("http://gmail.com");
+		  
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		  
+		driver.findElement(By.id("Email")).sendKeys("binayaranjan.jena");
+		driver.findElement(By.id("Passwd")).sendKeys("#emerge12345");
+		driver.findElement(By.id("signIn")).click();
+		
+		try{
+			driver.findElement(By.xpath("//span[b[text()='Indian Idol Voting Registration']]")).click();  
+		}catch(Throwable e){
+			driver.findElement(By.xpath("//span[contains(text(),'Indian Idol Voting Registration')]")).click();
+		}
+		  
+		driver.findElement(By.xpath("//span[contains(@class,'gbii')]")).click();
+		driver.findElement(By.id("gb_71")).click();
+		  
+		driver.quit();
+  }
+}
