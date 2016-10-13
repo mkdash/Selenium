5794b445da16ef886551ae7e6809cb8ba4ec6957
Mahendra Dash <mahendra.ku.dash@gmail.com>
2016-10-13 18:01:42 +0530

    A simple change

--- src/test/java/com/learn/selenium/gmail/login/LoginTest.java
+++ src/test/java/com/learn/selenium/gmail/login/LoginTest.java
@@ -12,6 +12,7 @@ public class LoginTest {
 	@Test
 	public void loginTest() {
 		// A simple test case - TBD
+		// Test Jenkins build trigger by a push.
 		driver = new FirefoxDriver();
 		driver.navigate().to("http://gmail.com");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
